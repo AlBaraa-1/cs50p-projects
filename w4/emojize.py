41a50deb83f427b17c4c59fb8ea9e76ef2881f50
AlBaraa Mohammad AlOlabi <666645@gmail.com>
2025-05-12 16:18:35 +0400

    solved emojize.py for Week 4 problem set

--- w4/emojize.py
+++ w4/emojize.py
@@ -1,2 +1,16 @@
+import emoji
+
 def main():
-    print("hello")
+    # Prompt the user for input
+    text  = input("Input: ")
+
+    # Convert the text to emoji
+    Emo = emoji.emojize(text, language='alias')
+
+    # Print the output
+    print("Output: "+ Emo)
+    
+
+
+if __name__ == "__main__":
+    main()
