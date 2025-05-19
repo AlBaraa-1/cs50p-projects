66d05de53e496e61917505e229631841542b2c18
AlBaraa Mohammad AlOlabi <666645@gmail.com>
2025-05-19 19:07:25 +0400

    Add week7 watch solution

---
+++ w7/Watch on YouTube/watch.py
@@ -0,0 +1,21 @@
+import re
+import sys
+
+
+def main():
+    # parse the input from the user and print the result
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    # regex to match the YouTube URL in the HTML string
+    url = r"^<iframe\ssrc=\"https?://(?:www\.)?youtube\.com/embed/([\w-]+)\"></iframe>$"
+
+    # search for the URL in the input string
+    src = re.search(url, s)
+    if src:
+        # return the YouTube URL in the format "https://youtu.be/VIDEO_ID"
+        return "https://youtu.be/" + src.group(1)
+
+if __name__ == "__main__":
+    main()
